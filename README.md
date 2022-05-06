7a95fa38fe89c71e52a358861f4ae30ef3442ffa
michaelsatumba <mjsatumba@gmail.com>
2022-05-06 10:55:34 -0700

    font color - school projects - skill bar

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
+
+
 ## Here it is
-
-https://mkeportfol.netlify.app/
-
-## Also Here
 
 https://mkeport.vercel.app/
 
@@ -40,15 +38,7 @@ If you want to display your own pictures then you have to replace the files at t
 
 ## 7. Finalising Resume
 
-Once all the formatting and data input is finalised, run `npm run build`. This will create a dedicated build folder.
-
-## 8. Host Resume Online
-
-1) Open the build folder in the finder screen
-2) Go to <a href="https://www.netlify.com/">Netlify</a> and set up a profile
-3) Go to the 'Sites Tab'
-4) Simply Drag and drop the 'Build' Folder into the designated upload space
-5) The app will begin deploying & the deployment URL will be presented!
+Once all the formatting and data input is finalised, deploy on vercel.
 
 🚀 BOOM, you are now LIVE!!!
 
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -23,19 +23,50 @@ const Portfolio = ({ data }) => {
         </div>
       );
     });
+    var schoolProjects = data.schoolProjects.map(function (schoolProjects) {
+      var projectImage = "images/portfolio/" + schoolProjects.image;
+      return (
+        <div key={schoolProjects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a href={schoolProjects.url} title={schoolProjects.title}>
+              <img alt={schoolProjects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{schoolProjects.title}</h5>
+                  <p>{schoolProjects.category}</p>
+                </div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
+            </a>
+          </div>
+        </div>
+      );
+    });
   }
 
   return (
     <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
+          <h1>Personal Projects.</h1>
 
           <div
             id="portfolio-wrapper"
             className="bgrid-quarters s-bgrid-thirds cf"
           >
             {projects}
+          </div>
+        </div>
+        <div className="twelve columns collapsed">
+          <h1>School Projects.</h1>
+
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {schoolProjects}
           </div>
         </div>
       </div>
