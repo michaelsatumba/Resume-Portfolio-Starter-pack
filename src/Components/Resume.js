c253c683dfb855f5ec8c84d23b1ea07d0cd20660
michaelsatumba <mjsatumba@gmail.com>
2025-12-31 10:30:43 -0800

    added cg creds

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Resume = ({ data }) => {
 	const [showDescription, setShowDescription] = React.useState({});
 	const [showDescription2, setShowDescription2] = React.useState(false);
+	const [showEducation, setShowEducation] = React.useState({});
 
 	const toggleDescription = (id) => {
 		setShowDescription((prevState) => ({
@@ -12,44 +13,110 @@ const Resume = ({ data }) => {
 		console.log(showDescription);
 	};
 
-	const handleClick = () => {
-		setShowDescription2(!showDescription2);
-	};
+	const toggleEducation = (id) => {
+        setShowEducation((prev) => ({
+            ...prev,
+            [id]: !prev[id],
+        }));
+    };
 
 	if (data) {
+		// var skillmessage = data.skillmessage;
+		// var education = data.education.map(function (education) {
+		// 	const idKey = education.id || education.school;
+		// 	return (
+		// 		<div key={idKey}>
+		// 			<h3>{education.school}</h3>
+		// 			{showEducation[idKey] ? (
+		// 				<div>
+		// 					<p className="info">
+		// 						{education.degree} <span>&bull;</span>
+		// 						<em className="date">{education.graduated}</em>
+		// 					</p>
+		// 					<p className="info">
+		// 						{education.degree2} <span>&bull;</span>
+		// 						<em className="date">{education.graduated}</em>
+		// 					</p>
+		// 					<p className="info">
+		// 						{education.degree3} <span>&bull;</span>
+		// 						<em className="date">{education.graduated2}</em>
+		// 					</p>
+		// 					<p className="info">{education.description}<span>&bull;</span><em className="date">{education.graduated2}</em></p>
+		// 					<p className="info">{education.description1}<span>&bull;</span><em className="date">Spring 2019</em></p>
+		// 					<p className="info">{education.description2}<span>&bull;</span><em className="date">Fall 2018</em></p>
+		// 					<img src="/images/gradCert.png" alt="graduateCert" loading="lazy"/>
+		// 					<img src="/images/masters.png" alt="masters" loading="lazy"/>
+		// 					<br />
+		// 					<button onClick={() => toggleEducation(idKey)}>Collapse</button>
+		// 				</div>
+		// 			) : (
+		// 				<button onClick={() => toggleEducation(idKey)}>Explore Further</button>
+		// 			)}
+		// 		</div>
+		// 	);
+		// });
 		var skillmessage = data.skillmessage;
-		var education = data.education.map(function (education) {
-			return (
-				<div key={education.school}>
-					<h3>{education.school}</h3>
-					{showDescription2 ? (
-						<div>
-							<p className="info">
-								{education.degree} <span>&bull;</span>
-								<em className="date">{education.graduated}</em>
-							</p>
-							<p className="info">
-								{education.degree2} <span>&bull;</span>
-								<em className="date">{education.graduated}</em>
-							</p>
-							<p className="info">
-								{education.degree3} <span>&bull;</span>
-								<em className="date">{education.graduated2}</em>
-							</p>
-							<p className="info">{education.description}<span>&bull;</span><em className="date">{education.graduated2}</em></p>
-							<p className="info">{education.description1}<span>&bull;</span><em className="date">Spring 2019</em></p>
-							<p className="info">{education.description2}<span>&bull;</span><em className="date">Fall 2018</em></p>
-							<img src="/images/gradCert.png" alt="graduateCert" loading="lazy"/>
-							<img src="/images/masters.png" alt="masters" loading="lazy"/>
-							<br />
-							<button onClick={handleClick}>Explore Further</button>
-						</div>
-					) : (
-						<button onClick={handleClick}>Explore Further</button>
-					)}
-				</div>
-			);
-		});
+		var education = data.education.map(function (ed) {
+            const idKey = ed.id || ed.school;
+            return (
+                <div key={idKey}>
+                    <h3>{ed.school}</h3>
+                    <p className="info">
+                        {ed.degree || ed.title || ""} <span>&bull;</span>
+                        <em className="date">{ed.graduated}</em>
+                    </p>
+                    {showEducation[idKey] ? (
+                    <div>
+                        {ed.degree2 && (
+                            <p className="info">
+                                {ed.degree2} <span>&bull;</span>
+                                <em className="date">{ed.graduated2 || ed.graduated || ''}</em>
+                            </p>
+                        )}
+                        {ed.degree3 && (
+                            <p className="info">
+                                {ed.degree3} <span>&bull;</span>
+                                <em className="date">{ed.graduated2 || ''}</em>
+                            </p>
+                        )}
+                        {ed.description && (
+                            <p className="info">
+                                {ed.description}
+                                {(ed.graduated2 || '') && <span> <span>&bull;</span> <em className="date">{ed.graduated2}</em></span>}
+                            </p>
+                        )}
+                        {ed.description1 && <p className="info">{ed.description1}</p>}
+                        {ed.description2 && <p className="info">{ed.description2}</p>}
+
+						{/* certificate image (shown when present) */}
+                        {ed.certificateImage && (
+                            <p className="info">
+                                <img
+                                    src={ed.certificateImage}
+                                    alt={`${ed.school} certificate`}
+                                    style={{ maxWidth: '100%', height: 'auto' }}
+                                    loading="lazy"
+                                />
+                            </p>
+                        )}
+
+                        {/* optional academic images for non-military entries */}
+                        {ed.school && !/coast guard/i.test(ed.school) && (
+                            <>
+                                <img src="/images/gradCert.png" alt="graduateCert" loading="lazy" />
+                                <img src="/images/masters.png" alt="masters" loading="lazy" />
+                            </>
+                        )}
+
+                        <br />
+                        <button onClick={() => toggleEducation(idKey)}>Collapse</button>
+                    </div>
+                ) : (
+                    <button onClick={() => toggleEducation(idKey)}>Explore Further</button>
+                )}
+                </div>
+            );
+        });
 		var work = data.work.map(function (work) {
 			return (
 				<div key={work.id}>
@@ -60,8 +127,8 @@ const Resume = ({ data }) => {
 					</p>
 					{showDescription[work.id] ? <p>{work.description}</p> : null}
 					<button onClick={() => toggleDescription(work.id)}>
-					Explore Further
-					</button>
+                        {showDescription[work.id] ? 'Collapse' : 'Explore Further'}
+                    </button>
 				</div>
 			);
 		});
